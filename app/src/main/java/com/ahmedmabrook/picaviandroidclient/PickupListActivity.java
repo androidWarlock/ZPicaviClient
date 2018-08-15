04c8d184a51a8f4435b77b3d60dd8b2cd0cc2c69
zMabrook <techboy.mabrouk@gmail.com>
2018-08-15 06:08:14 +0200

    pickup screen UI added along with list item UI

---
+++ app/src/main/java/com/ahmedmabrook/picaviandroidclient/PickupListActivity.java
@@ -0,0 +1,13 @@
+package com.ahmedmabrook.picaviandroidclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickupListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup_list);
+    }
+}
